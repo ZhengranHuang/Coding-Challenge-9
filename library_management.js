d294457dbc420fd3a176196df50d2348e6d4d495
ZhengranHuang <Zhengran@usf.edu>
2024-10-18 20:39:20 -0400

    Create Book class with properties and methods

---
+++ library_management.js
@@ -0,0 +1,25 @@
+//Task 1:Create a Book Class
+class Book{
+constructor(title, author, ISBN){
+this.title = title;
+this.author = author;
+this.ISBN = ISBN;
+this._isAvailable = true;
+}
+getDetails(){
+    return `Title: ${this.title}, Author: ${this.author}, ISBN: ${this.ISBN}`;
+}
+//GETTER
+get isAvailable() {
+    return this._isAvailable;
+}
+//SETTER
+set isAvailable(trueorfalse){
+if(typeof trueorfalse === 'string'&&(trueorfalse === 'true' || trueorfalse === 'false')){
+this._isAvailable = trueorfalse
+}
+else{
+console.error("TRUE OR FALSE NEEDED");
+}
+}
+}
